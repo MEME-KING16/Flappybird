931154f2fda3f1bcba730e49ca9a0d338206219d
Mini188 Cours2025 <ENGGm617@uottawa.ca>
2025-04-29 11:00:26 -0400

    added globals and evntlstener

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-console.warn("DONT ENTER ANY CODE YOU DONT UNDERSTAND YOU CAN DO THIS COOL THING CALLED XXS AND YOU WILL BE COOKED")
+console.warn("DONT ENTER ANY CODE YOU DONT UNDERSTAND YOU CAN DO THIS COOL THING CALLED XXS AND YOU WILL BE COOKED");
+let g = .25;
+let bird_y = 0;
+let score = 0;
+let game_stat = "start";
+
+let bird_elm = document.getElementById("bird");
+let score_display = document.getElementById("score");
+let game_cont = document.getElementById("game-container");
+let start_btn = document.getElementById("start-btn");
+
+start_btn.addEventListener("click", ()=>{
+    start()
+})
+
 
 function start() {
     alert("Game started") // Add code
