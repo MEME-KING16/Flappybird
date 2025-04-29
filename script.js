c23c9d89c286c652fca6b45997b0c7d850a65d0b
Mini188 Cours2025 <ENGGm617@uottawa.ca>
2025-04-29 14:27:16 -0400

    STUFF CHECK UR SELF IM TO LAZY TO TELL YOU CHILD

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-console.warn("DONT ENTER ANY CODE YOU DONT UNDERSTAND YOU CAN DO THIS COOL THING CALLED XXS AND YOU WILL BE COOKED");
+console.warn(
+  "DONT ENTER ANY CODE YOU DONT UNDERSTAND YOU CAN DO THIS COOL THING CALLED XXS AND YOU WILL BE COOKED"
+);
 // Globals
-let g = .25;
+let g = 0.25;
 let bird_dy = 0;
 let score = 0;
 let game_state = "start";
-
+let mode = "nightmare";
 
 // Interval
 let gameInterval = null;
@@ -16,32 +18,58 @@ let game_cont = document.getElementById("game-container");
 let start_btn = document.getElementById("start-btn");
 
 // Start evnt lstenr
-start_btn.addEventListener("click", ()=>{
-    start();
-    start_btn.style.display = "none";
-})
-
-//Flap evnt lstenr
-document.addEventListener("keydown", (e)=>{
-    if (e.code === "Space") {
-        if (game_state !== "play") {
-            start()
-        }
-    }
+start_btn.addEventListener("click", () => {
+  start();
+  start_btn.style.display = "none";
 });
 
-function applyGravity() {
-    bird_dy += g;
-    let birdTop = bird_elm.offsetTop + bird_dy;
+//Flap evnt lstenr
+document.addEventListener("keydown", (e) => {
+    e.preventDefault()
+  if (e.code === "Space") {
+    if (game_state !== "play") {
+      start();
+    }
+    bird_dy = -7;
+  }
+});
 
-    birdTop = Math.max(birdTop, 0);
-    birdTop = Math.min(birdTop, 682);
-    bird_elm.style.top = birdTop + 'px';
+document.addEventListener("click", (e) => {
+    e.preventDefault()
+      if (game_state !== "play") {
+        start();
+      }
+      bird_dy = -7;
+  });
+
+function applyGravity() {
+  bird_dy += g;
+  let birdTop = bird_elm.offsetTop + bird_dy;
+  bird_elm.classList.remove("jump");
+  bird_elm.classList.remove("fall");
+  if (bird_dy < 0) {
+    bird_elm.classList.add("jump");
+  } else if (bird_dy > 0) {
+    bird_elm.classList.add("fall");
+  }
+  birdTop = Math.max(birdTop, -20);
+  birdTop = Math.min(birdTop, 682);
+  bird_elm.style.top = birdTop + "px";
 }
 
 function start() {
-    game_state = "play";
-    gameInterval = setInterval(() => {
-        applyGravity();
-    }, 100);
+  if (mode === "nightmare") {
+    game_cont.classList.add("nightmareMode");
+    bird_elm.classList.add("nightmareMode");
+  } else {
+    game_cont.classList.remove("nightmareMode");
+    bird_elm.classList.remove("nightmareMode");
+  }
+  game_state = "play";
+  start_btn.style.display = "none";
+  score_display.classList.add("started");
+  if (gameInterval !== null) return;
+  gameInterval = setInterval(() => {
+    applyGravity();
+  }, 10);
 }
