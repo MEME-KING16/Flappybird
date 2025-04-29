c9eb4e92dea4cc4d68644afa0ff26d4a2b964a36
Mini188 Cours2025 <ENGGm617@uottawa.ca>
2025-04-29 11:03:48 -0400

    comments and gameInterval

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 console.warn("DONT ENTER ANY CODE YOU DONT UNDERSTAND YOU CAN DO THIS COOL THING CALLED XXS AND YOU WILL BE COOKED");
+// Globals
 let g = .25;
 let bird_y = 0;
 let score = 0;
 let game_stat = "start";
 
+
+// Interval
+let gameInterval = null
+
+// Elements
 let bird_elm = document.getElementById("bird");
 let score_display = document.getElementById("score");
 let game_cont = document.getElementById("game-container");
 let start_btn = document.getElementById("start-btn");
 
+// Start evnt lstenr
 start_btn.addEventListener("click", ()=>{
     start()
 })
